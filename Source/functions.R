1c13b02763fbe90c5797266d7b33eda4920e2d54
luyoutao <luyoutao@sas.upenn.edu>
2025-02-10 20:29:41 -0500

    corrected wrong license headers (Artistic to CC)

--- Source/functions.R
+++ Source/functions.R
@@ -4,8 +4,7 @@
 ## Copyright (c) 2021-2022, Youtao Lu and Junhyong Kim, Department of Biology, University of Pennsylvania
 ## Copyright (c) 2021-2022, Jai-Yoon Sul, Department of Systems Pharmacology and Translational Therapeutics, University of Pennsylvania
 ## 
-## This program is free software; you can redistribute it and/or modify it under the terms of the the Artistic License (2.0). You may obtain a copy of the full license at: 
-## https://opensource.org/license/artistic-2-0/
+## This Source Code is distributed under Creative Commons Attribution License 4.0 (CC BY).
 ###########################################################################
 source("Source/functions/Repo.R")
 source("Source/functions/Tools.R")
